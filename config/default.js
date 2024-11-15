5297de4772eaaaa920d3d3917c9accea6c6bbf4b
elmoustafi-22 <107369052+Elmoustafi-22@users.noreply.github.com>
2024-11-15 22:21:57 +0100

    Login API created ✌

---
+++ config/default.js
@@ -0,0 +1,6 @@
+module.exports = {
+  port: 8000,
+  dbURI:
+    "mongodb+srv://abdulqadirmustopha:6uKBts2SS5JsOVDH@rest-api-login-registra.8ecmg.mongodb.net/?retryWrites=true&w=majority&appName=rest-api-login-registration",
+  saltFactor: 12,
+};
